5126526248eb7a30a253ee151584805455a92afb
Andrew Skinner <askinner@comlinx.com.au>
2021-04-04 10:31:40 +1000

    added more doc strings

--- vcoclient.py
+++ vcoclient.py
@@ -56,22 +56,25 @@ class VcoClient:
         return timestamp.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 
 
-    # Gets a list of enterprises managed by a partner
-    def get_enterprise_proxy_enterprises(self):
+    def get_enterprise_proxy_enterprises(self) -> list:
+        """
+        Returns a list of Enterprises associated with an EnterpriseProxy (MSP/Partner)
+
+        Returns:
+            (list) : A list of Enterprise dicts
+        """
         resp = self.request('enterpriseProxy/getEnterpriseProxyEnterprises', {})
         return resp.json()
 
-    # Gets a list of edges within an enterprise and returns a dict
-    def get_enterprise_edges(self, enterprise_id: int = 0) -> object:
-        if enterprise_id == 0:
-            body = {}
-        else:
-            body = {"enterpriseId" : enterprise_id}
+    # Gets a list of edges within an enterprise and returns a list
+    def get_enterprise_edges(self, enterprise_id: int = 0) -> list:
+        body = {} if enterprise_id == 0 else {"enterpriseId" : enterprise_id}
+
         resp = self.request('enterprise/getEnterpriseEdges', body)
         return resp.json()
 
     # collects TSD data for an edge
-    def get_edge_link_series(self, enterprise_id: int, edge_id: int, **kwargs) -> object:
+    def get_edge_link_series(self, enterprise_id: int, edge_id: int, **kwargs) -> list:
         """
         Returns a python object containing the link series data for an edge during a given interval
 
@@ -109,20 +112,20 @@ class VcoClient:
         return resp.json()
 
     # Collects a list of identifiable apps from the VCO
-    def get_identifiable_applications(self, enterprise_id):
+    def get_identifiable_applications(self, enterprise_id: int) -> list:
         resp = self.request('configuration/getIdentifiableApplications',
                             {"enterpriseId": enterprise_id})
         return resp.json()
 
             # Collects a list of identifiable apps from the VCO
-    def get_edge_configuration_stack(self, enterprise_id: int, edge_id: int):
+    def get_edge_configuration_stack(self, enterprise_id: int, edge_id: int) -> list:
         resp = self.request('configuration/getIdentifiableApplications',
                             {"enterpriseId": enterprise_id, "edgeId": edge_id})
         return resp.json()
 
         # Gets a list of application TSD
 
-    def get_edge_app_series(self, enterpriseId, edgeId, **kwargs):
+    def get_edge_app_series(self, enterprise_id: int, edge_id: int, **kwargs) -> list:
         # Define the interval that we're pulling stats for
         interval = {"start": kwargs.get("start",
                                         self.make_orchestrator_timestamp(
@@ -136,23 +139,23 @@ class VcoClient:
                                       )
                     }
         # Create the HTTP Request Body
-        body = {"edgeId" : edgeId, "interval" : interval,
+        body = {"edgeId" : edge_id, "interval" : interval,
                 "resolveApplicationNames": True, "limit" : -1}
-        if enterpriseId != 0 :
-            body['enterpriseId'] = enterpriseId
+        if enterprise_id != 0:
+            body['enterpriseId'] = enterprise_id
 
         if kwargs.get("metrics") is not None:
             body['metrics'] = kwargs['metrics']
 
         if kwargs.get("applications") is not None:
-            body['applications'] =kwargs['applications']
+            body['applications'] = kwargs['applications']
 
         resp = self.request('metrics/getEdgeAppSeries', body)
         return resp.json()
 
 
     # Gets a list of application metrics
-    def get_edge_app_metrics(self, enterpriseId, edgeId, **kwargs):
+    def get_edge_app_metrics(self, enterprise_id: int, edge_id: int, **kwargs) -> list:
         # Define the interval that we're pulling stats for
         interval = {"start": kwargs.get("start",
                                         self.make_orchestrator_timestamp(
@@ -166,10 +169,10 @@ class VcoClient:
                                       )
                     }
         # Create the HTTP Request Body
-        body = {"edgeId" : edgeId, "interval" : interval,
+        body = {"edgeId" : edge_id, "interval" : interval,
                 "resolveApplicationNames": True, "limit" : -1}
-        if enterpriseId != 0 :
-            body['enterpriseId'] = enterpriseId
+        if enterprise_id != 0:
+            body['enterpriseId'] = enterprise_id
 
         if kwargs.get("metrics") is not None:
             body['metrics'] = kwargs['metrics']
@@ -179,6 +182,6 @@ class VcoClient:
 
         # Get a list of link metrics
 
-    def get_link_quality_events(self, enterpriseId, edgeId, **kwargs):
+    def get_link_quality_events(self,  edgeId, enterpriseId, **kwargs):
         pass
 
